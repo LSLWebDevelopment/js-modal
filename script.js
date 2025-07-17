5414ffe4bd77068b22aa8cebedea5f0ece486b89
LSL Web Development <lslwebdevelopment@gmail.com>
2025-07-17 12:34:17 -0300

    Refactor the code

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 'use strict';
 
-function openModal() {
-  document.getElementsByClassName('modal')[0].style.display = 'block';
-  document.getElementsByClassName('overlay')[0].style.display = 'block';
+function openCloseModal() {
+  document.querySelector('.modal').classList.toggle('hidden');
+  document.querySelector('.overlay').classList.toggle('hidden');
 }
 
-function closeModal() {
-  document.getElementsByClassName('modal')[0].style.display = 'none';
-  document.getElementsByClassName('overlay')[0].style.display = 'none';
-}
-
-const showModalButtons = document.getElementsByClassName('show-modal');
+const showModalButtons = document.querySelectorAll('.show-modal');
 for (let i = 0; i < showModalButtons.length; i++) {
   showModalButtons[i].addEventListener('click', () => {
-    console.log(`Button ${i + 1} clicked!`);
-    openModal();
+    openCloseModal();
   });
 }
 
 const closeModalButton = document.querySelector('.close-modal');
 closeModalButton.addEventListener('click', () => {
-  closeModal();
+  openCloseModal();
 });
 
 const overlay = document.querySelector('.overlay');
 overlay.addEventListener('click', () => {
-  closeModal();
+  openCloseModal();
 });
 
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape') {
-    closeModal();
+    openCloseModal();
   }
 });
